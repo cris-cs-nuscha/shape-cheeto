1fb08f8c3d2ba1dcf458fba82662c32765c172ef
nuscha <nuscha.p@cris.ac.th>
2024-04-29 12:08:37 +0700

    add shapes and modified shapes to developed the house picture

--- hot_cheeto.py
+++ hot_cheeto.py
@@ -4,7 +4,7 @@ Nuscha Phraaruk
 I will code my program to draw many different shapes with different colors,
 sizes, and positions. the instrunctions for each shapes will be read from a file 
 """
-
+from random import*
 import turtle
 import sys
 
@@ -35,7 +35,7 @@ def print_error(msg):
     print(msg, file=sys.stderr)
 
 # shape handler functions
-def draw_square(t, data):
+def draw_rectangle(t, data):
     '''Draws a square on given turtle object and data dictionary'''
     print("pendown:", t.isdown())
     t.setpos(int(data['pos_x']), int(data['pos_y']))
@@ -43,28 +43,33 @@ def draw_square(t, data):
     t.fillcolor(data['fill']) 
     t.down()
     t.begin_fill()
-    side = float(data["sizel"])
-    t.forward(side)
+    side1 = float(data["size1"])
+    side2 = float(data["size2"])
+    t.forward(side1)
     t.left(90)
-    t.forward(side)
+    t.forward(side2)
     t.left(90)
-    t.forward(side)
+    t.forward(side1)
     t.left(90)
-    t.forward(side)
+    t.forward(side2)
     t.left(90)
     t.end_fill()
     t.up()
 
-
+def draw_square(t, data):
+    """Use draw_rectangle to draw a square"""
+    data['size2'] = data['size1']
+    draw_rectangle(t, data)
 
 def draw_triangle(t, data):
     '''Draws a square on given turtle object and data dictionary'''
     print("pendown:", t.isdown())
     t.setpos(int(data['pos_x']), int(data['pos_y']))
-    t.color(data['color']) 
+    t.color(data['color'])
+    t.fillcolor(data['fill']) 
     t.down()
     t.begin_fill()
-    side = float(data["sizel"])
+    side = float(data["size1"])
     t.forward(side)
     t.left(120)
     t.forward(side)
@@ -78,20 +83,30 @@ def draw_triangle(t, data):
 def draw_circle(t, data):
     '''Draws a circle on given turtle object and data dictionary'''
     print(data)
+    t.setpos(int(data['pos_x']), int(data['pos_y']))
+    t.pencolor(data['color'])
+    t.fillcolor(data['fill'])
     t.down()
-    t.pencolor(data['color']) 
-    radius = float(data["sizel"])
+    t.begin_fill()
+    radius = float(data["size1"])
     t.color(data['color']) 
     t.circle(radius)
     '''Draws a square on given turtle object and data dictionary'''
     print("pendown:", t.isdown())
-    t.setpos(int(data['pos_x']), int(data['pos_y']))
+    t.end_fill()
     t.up()
 
 
+'''
+#set up the area for turtle to ues 
+area = Screen()
+area.setuo(500,500)
+area.bgcolor('white')
+'''
 
 # dictionary of shapes and callback functions 
 handlers = {
+    'rectangle': draw_rectangle,
     'square': draw_square,
     'circle': draw_circle,
     'triangle': draw_triangle
@@ -108,8 +123,9 @@ def get_handler(shape_type):
 
 
 # create turtle object and Screen
-win = turtle.Screen()
 t = turtle.Turtle()
+win = turtle.Screen()
+
 #win.bgpic("grid.png")
 t.up()
 
